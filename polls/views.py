37aeedd615880d93e912b602f4f70e287ecc7d3f
hakimled <hakimled30@gmail.com>
2023-10-14 09:51:30 +0100

    New changes added to the voting system  between the best soccer players in the world

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+import socket
+
 
 
 def home(request):
     
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    context = {'ip': ip}
     
-    return render(request, 'polls/polls.html')
+    
+    return render(request, 'polls/polls.html' , context)
